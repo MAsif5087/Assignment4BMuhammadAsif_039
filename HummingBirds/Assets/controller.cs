f98440d50d79fecc52ca07a49dcc5b123b8de7c7
MAsif5087 <fa19-rcs-039@student.comsats.edu.pk>
2020-07-13 02:00:41 +0500

    update assignment4b

---
+++ HummingBirds/Assets/controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class controller : MonoBehaviour
+{
+    public void playPenguine()
+    {
+
+        SceneManager.LoadScene("Penguins");
+    }
+
+    public void playHummingBird()
+    {
+
+        SceneManager.LoadScene("FlowerIseland");
+    }
+}
